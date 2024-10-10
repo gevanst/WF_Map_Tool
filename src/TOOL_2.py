68de551c0bf9c6e38ebac4047ed90b3f360841ec
gevanst <gtevans@hawaii.edu>
2024-10-10 08:50:32 -1000

    adding more layers back in

--- src/TOOL_2.py
+++ src/TOOL_2.py
@@ -123,7 +123,7 @@ layer_info = {
                 'Reef Flat': 'pink',
                 'Land': 'gray',
                 'Unknown': 'black'
-            }.get(feature['properties']['ZONE'], 'brown'),
+            }.get(feature['properties'].get('ZONE', 'Unknown'), 'brown'),
             'color': 'black',
             'weight': 1,
             'fillOpacity': 0.4
@@ -133,38 +133,289 @@ layer_info = {
         'name': 'Benthic Habitats',
         'simplify_tolerance': 0.001  # Adjust as needed
     },
-    # Add other layers similarly...
+    'Coral Reefs (Nautical Charts)': {
+        'file_path': 'data/CoralReefs_nc.shp',
+        'style_function': lambda feature: {
+            'fillColor': 'teal',
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.6
+        },
+        'tooltip_fields': ["acres", "chart1", "chart2", "notes"],
+        'aliases': ["Area (Acres)", "Source Chart 1", "Source Chart 2", "Notes"],
+        'name': 'Coral Reefs (Nautical Charts)',
+        'simplify_tolerance': 0.001
+    },
+    'Whale Sanctuary': {
+        'file_path': 'data/sanctuary.shp',
+        'style_function': lambda feature: {
+            'fillColor': 'lightblue',
+            'color': 'blue',
+            'weight': 1,
+            'fillOpacity': 0.5
+        },
+        'tooltip_fields': ["sanctuary", "Shape_Leng", "Shape_Area"],
+        'aliases': ["Sanctuary Name", "Perimeter (m)", "Area (sq. m)"],
+        'name': 'Whale Sanctuary',
+        'simplify_tolerance': 0.001
+    },
+    'Ocean Depth': {
+        'file_path': 'data/Ocean_Depth.shp',
+        'style_function': lambda feature: {
+            'fillColor': {
+                1000: '#BFEFFF',
+                2000: '#7EC0EE',
+                3000: '#4682B4',
+                4000: '#104E8B',
+                5000: '#000080'
+            }.get(feature['properties'].get('DEPTH', 5000), '#708090'),
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.5
+        },
+        'tooltip_fields': ["DEPTH"],
+        'aliases': ["Depth (m)"],
+        'name': 'Ocean Depth',
+        'simplify_tolerance': 0.001
+    },
+    'Dumping Areas': {
+        'file_path': 'data/DumpingAreas.shp',
+        'style_function': lambda feature: {
+            'fillColor': 'brown',
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.6
+        },
+        'tooltip_fields': ["id", "chart", "cfr", "notes"],
+        'aliases': ["Polygon ID", "Chart Source", "Regulation", "Notes"],
+        'name': 'Dumping Areas',
+        'simplify_tolerance': 0.001
+    },
+    'Moku Ridge to Reef': {
+        'file_path': 'data/moku_ridge_to_reef_dar.shp',
+        'style_function': lambda feature: {
+            'fillColor': {
+                'Hilo': 'orange',
+                'Kona': 'red',
+                'Puna': 'yellow',
+                'Kohala': 'green',
+                'Kau': 'blue',
+                'Molokai': 'purple',
+                'Oahu': 'brown',
+                'Maui': 'cyan'
+            }.get(feature['properties'].get('Moku'), 'gray'),
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.6
+        },
+        'tooltip_fields': ["Moku_ID", "Moku", "Mokupuni"],
+        'aliases': ["Moku ID", "Moku", "Island"],
+        'name': 'Moku Ridge to Reef',
+        'simplify_tolerance': 0.001
+    },
+    'Submarine Cables': {
+        'file_path': 'data/Cables.shp',
+        'style_function': lambda feature: {
+            'color': 'blue',
+            'weight': 3,
+            'opacity': 0.7,
+            'dashArray': '5, 5' if 'Descript' in feature['properties'] and 'Proposed' in feature['properties']['Descript'] else None
+        },
+        'tooltip_fields': ["id", "length", "descriptio", "chart1", "chart2"],
+        'aliases': ["Cable ID", "Length (m)", "Description", "Primary Chart", "Secondary Chart"],
+        'name': 'Submarine Cables',
+        'simplify_tolerance': 0.001
+    },
+    'Unexploded Ordnance': {
+        'file_path': 'data/UnexplodedOrdnance.shp',
+        'marker_color': 'red',
+        'tooltip_fields': ["id", "notes", "chart"],
+        'aliases': ["ID", "Notes", "Chart"],
+        'name': 'Unexploded Ordnance',
+    },
+    'Explosive Dumping Areas': {
+        'file_path': 'data/ExplosiveDumpingAreas.shp',
+        'style_function': lambda feature: {
+            'fillColor': 'orange',
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.6
+        },
+        'tooltip_fields': ["chart", "notes", "acres"],
+        'aliases': ["Source Chart", "Notes", "Area (Acres)"],
+        'name': 'Explosive Dumping Areas',
+        'simplify_tolerance': 0.001
+    },
+    'Sewer Lines': {
+        'file_path': 'data/sewerlines.shp',
+        'style_function': lambda feature: {
+            'color': 'brown',
+            'weight': 3,
+            'opacity': 0.7,
+        },
+        'tooltip_fields': ["id", "notes", "island", "chart"],
+        'aliases': ["Sewer ID", "Notes", "Island", "Chart"],
+        'name': 'Sewer Lines',
+        'simplify_tolerance': 0.001
+    },
+    'Submerged Buoys': {
+        'file_path': 'data/SubmergedBuoys.shp',
+        'marker_color': 'purple',
+        'tooltip_fields': ["name", "notes"],
+        'aliases': ["ID", "Description"],
+        'name': 'Submerged Buoys',
+    },
+    'Navigation Aids': {
+        'file_path': 'data/NavigationAids.shp',
+        'marker_color': 'darkgreen',
+        'tooltip_fields': ["id", "type", "name"],
+        'aliases': ["ID", "Type", "Name"],
+        'name': 'Navigation Aids',
+    },
+    'Obstructions': {
+        'file_path': 'data/obstructions.shp',
+        'marker_color': 'orange',
+        'tooltip_fields': ["id", "notes"],
+        'aliases': ["ID", "Notes"],
+        'name': 'Obstructions',
+    },
+    'Fish Aggregation Devices (FADs)': {
+        'file_path': 'data/fads_nmfs.shp',
+        'marker_color': 'blue',
+        'tooltip_fields': ["name", "island"],
+        'aliases': ["ID", "Description"],
+        'name': 'Fish Aggregation Devices (FADs)',
+    },
+    'City Zoning': {
+        'file_path': 'data/cty_zoning_mau.shp',
+        'style_function': lambda feature: {
+            'fillColor': {
+                'R-3 Residential': 'lightblue',
+                'AP Airport': 'orange',
+                'AG Agricultural': 'green',
+                'INT Interim': 'brown',
+                'PK Park': 'red',
+                'M-2 Heavy Industrial': 'gray',
+                'H-2 Hotel': 'purple',
+                'M-1 Light Industrial': 'yellow',
+                'B-2 Business-Community': 'cyan'
+            }.get(feature['properties'].get('ZONE_CLASS'), 'black'),
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.6,
+        },
+        'tooltip_fields': ['ZONE_CLASS', 'CP_AREA'],
+        'aliases': ['Zoning Class:', 'Community Plan Area:'],
+        'name': 'City Zoning',
+        'simplify_tolerance': 0.001
+    },
+    'False Killer Whale Habitat': {
+        'file_path': 'data/WhaleFalseKiller_MainHawaiianIslandsInsularDPS_20180724.shp',
+        'style_function': lambda feature: {
+            'fillColor': {
+                'marine': 'blue',
+                'nearshore': 'cyan',
+                'offshore': 'purple',
+                'unknown': 'gray'
+            }.get(feature['properties'].get('HABTYPE'), 'lightgray'),
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.5,
+        },
+        'tooltip_fields': ['SCIENAME', 'COMNAME', 'HABTYPE', 'AREASqKm'],
+        'aliases': ['Species Name', 'Common Name', 'Habitat Type', 'Area (sq. km)'],
+        'name': 'False Killer Whale Habitat',
+        'simplify_tolerance': 0.001
+    },
+    'Monk Seal Habitat': {
+        'url': "https://services2.arcgis.com/C8EMgrsFcRFL6LrL/arcgis/rest/services/SealHawaiianMonk_20150821_line/FeatureServer/59/query",
+        'params': {
+            "where": "1=1",
+            "outFields": "*",
+            "f": "geojson"
+        },
+        'style_function': lambda feature: {
+            'color': 'red',
+            'weight': 3,
+            'opacity': 0.7,
+        },
+        'tooltip_fields': ['ID', 'COMNAME', 'HABTYPE', 'AREASqKm'],
+        'aliases': ['ID', 'Common Name', 'Habitat Type', 'Area (sq. km)'],
+        'name': 'Monk Seal Habitat',
+    },
 }
 
-selected_layers = st.sidebar.multiselect('Select layers to display:', list(layer_info.keys()))
+selected_layers = st.sidebar.multiselect('Select layers to display:', list(layer_info.keys()), default=list(layer_info.keys()))
 
 # Function to load and process layers
 @st.cache_data
 def load_layer(layer_key):
     info = layer_info[layer_key]
-    gdf = gpd.read_file(info['file_path'])
-    gdf = gdf.to_crs("EPSG:4326")
-    if 'simplify_tolerance' in info:
-        gdf['geometry'] = gdf['geometry'].simplify(info['simplify_tolerance'])
-    geojson_data = gdf.to_json(drop_id=True, double_precision=3)
-    return geojson_data
+    if 'file_path' in info:
+        gdf = gpd.read_file(info['file_path'])
+        gdf = gdf.to_crs("EPSG:4326")
+        if 'simplify_tolerance' in info:
+            gdf['geometry'] = gdf['geometry'].simplify(info['simplify_tolerance'])
+        if gdf.geometry.is_empty.any():
+            gdf = gdf[~gdf.geometry.is_empty]
+        geojson_data = gdf.to_json(drop_id=True, double_precision=3)
+        return geojson_data, None
+    elif 'url' in info:
+        response = requests.get(info['url'], params=info['params'])
+        geojson_data = response.json()
+        return geojson_data, None
+    else:
+        return None, None
+
+# Function to add point layers
+def add_point_layer(gdf, layer_name, marker_color, tooltip_fields, aliases):
+    feature_group = folium.FeatureGroup(name=layer_name)
+    for _, row in gdf.iterrows():
+        if row.geometry and row.geometry.is_valid:
+            coords = [row.geometry.y, row.geometry.x]
+            tooltip_content = "<br>".join(
+                f"{alias}: {row[field]}" for field, alias in zip(tooltip_fields, aliases)
+            )
+            folium.CircleMarker(
+                location=coords,
+                radius=6,
+                color=marker_color,
+                fill=True,
+                fill_color=marker_color,
+                fill_opacity=0.7,
+                popup=folium.Popup(tooltip_content, max_width=250)
+            ).add_to(feature_group)
+    feature_group.add_to(hawaii_map)
 
 # Add selected layers to the map
 for layer_name in selected_layers:
     info = layer_info[layer_name]
-    geojson_data = load_layer(layer_name)
-    feature_group = folium.FeatureGroup(name=layer_name)
-    folium.GeoJson(
-        geojson_data,
-        name=layer_name,
-        style_function=info['style_function'],
-        tooltip=folium.GeoJsonTooltip(
-            fields=info['tooltip_fields'],
-            aliases=info['aliases'],
-            localize=True
-        )
-    ).add_to(feature_group)
-    feature_group.add_to(hawaii_map)
+    geojson_data, _ = load_layer(layer_name)
+    if geojson_data:
+        feature_group = folium.FeatureGroup(name=layer_name)
+        if 'marker_color' in info:
+            # Handle point layers
+            gdf = gpd.read_file(info['file_path'])
+            gdf = gdf.to_crs("EPSG:4326")
+            add_point_layer(
+                gdf,
+                layer_name,
+                info['marker_color'],
+                info['tooltip_fields'],
+                info['aliases']
+            )
+        else:
+            folium.GeoJson(
+                geojson_data,
+                name=layer_name,
+                style_function=info['style_function'],
+                tooltip=folium.GeoJsonTooltip(
+                    fields=info['tooltip_fields'],
+                    aliases=info['aliases'],
+                    localize=True
+                )
+            ).add_to(feature_group)
+            feature_group.add_to(hawaii_map)
 
 # Add LayerControl to enable toggling
 folium.LayerControl().add_to(hawaii_map)
@@ -193,13 +444,13 @@ def layout_turbines(bounds, spacing, single_turbine=False):
     lat_spacing = spacing / 110000
 
     # Calculate the number of turbines that fit, rounding down
-    num_turbines_x = math.floor((max_lon - min_lon) / lon_spacing)
-    num_turbines_y = math.floor((max_lat - min_lat) / lat_spacing)
+    num_turbines_x = max(1, math.floor((max_lon - min_lon) / lon_spacing))
+    num_turbines_y = max(1, math.floor((max_lat - min_lat) / lat_spacing))
 
     turbine_positions = []
     for i in range(num_turbines_x):
         for j in range(num_turbines_y):
-            # Place turbines based on grid index and spacing, rounding down
+            # Place turbines based on grid index and spacing
             turbine_lon = min_lon + i * lon_spacing
             turbine_lat = min_lat + j * lat_spacing
             turbine_positions.append((turbine_lon, turbine_lat))
@@ -235,7 +486,7 @@ with info_container:
             gdf = gdf.to_crs(epsg=32604)  # Convert to UTM Zone 4 for accurate area calculation
             # Calculate area in square meters and convert to square kilometers
             area_sq_km = gdf.geometry.area.iloc[0] / 1e6
-            # Calculate the dimensions (length and width) of the rectangle in kilometers
+            # Calculate the dimensions (length and width) of the rectangle in meters
             width_m = gdf.geometry.iloc[0].bounds[2] - gdf.geometry.iloc[0].bounds[0]
             height_m = gdf.geometry.iloc[0].bounds[3] - gdf.geometry.iloc[0].bounds[1]
 
